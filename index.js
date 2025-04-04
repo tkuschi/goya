3438cafa28cfbbbbec61c4ed3a2e3e1314c53d15
tkuschi <153780789+tkuschi@users.noreply.github.com>
2025-04-04 23:20:17 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
+<script src="https://cdnjs.cloudflare.com/ajax/libs/gsap/3.10.4/gsap.min.js"></script>
+<script src="https://unpkg.com/mouse-follower@1/dist/mouse-follower.min.js"></script>
+<script>
+    const cursor = new MouseFollower({
+    container: document.body,
+    speed: 0.3
+});
+</script>
 
+<link rel="stylesheet" href="https://unpkg.com/mouse-follower@1/dist/mouse-follower.min.css">
+
+<script src="https://unpkg.com/mouse-follower@1/dist/mouse-follower.min.js"></script>	
